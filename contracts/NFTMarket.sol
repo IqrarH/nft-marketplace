da0d49d17683dcff00132856dac893cdffd7b42b
IqrarH <iqrarhussain471@gmail.com>
2022-02-25 19:39:10 +0500

    Constructor & Struct added

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -3,3 +3,26 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint listingPrice = 0.025 ether;    //As we'll be deploying to Polygon so this price will be in MATIC
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+}
