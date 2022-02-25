4cfefda158d2203b8aa5349a363034f452feb986
IqrarH <iqrarhussain471@gmail.com>
2022-02-25 18:39:21 +0500

    hardhat configured for munbai testnet & mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({path: './env'})
+const projectId = process.env.projectId;
 
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`
+    },
+  },
   solidity: "0.8.4",
 };
