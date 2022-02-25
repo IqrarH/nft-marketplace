990662b2d50bebcaba36039b6a030803f0961b34
IqrarH <iqrarhussain471@gmail.com>
2022-02-25 20:14:00 +0500

    createMarketItem function added

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -10,7 +10,7 @@ contract NFTMarket is ReentrancyGuard {
     Counters.Counter private _itemsSold;
 
     address payable owner;
-    uint listingPrice = 0.025 ether;    //As we'll be deploying to Polygon so this price will be in MATIC
+    uint256 listingPrice = 0.025 ether;    //As we'll be deploying to Polygon so this price will be in MATIC
 
     constructor(){
         owner = payable(msg.sender);
@@ -25,4 +25,63 @@ contract NFTMarket is ReentrancyGuard {
         uint256 price;
         bool sold;
     }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be at least 1 wei");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),  //Empty Address, Nobody owns this NFT when it is being created
+            price,
+            false
+        );
+
+        //transferFrom(from, to, token)
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
+    }
+
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId
+    ) public payable nonReentrant {
+        
+    }
+
 }
