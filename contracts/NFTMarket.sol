cbea48fbc30ce2df333691d64a7c72c417596fb3
IqrarH <iqrarhussain471@gmail.com>
2022-02-25 20:22:30 +0500

    create sale method added

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -81,7 +81,17 @@ contract NFTMarket is ReentrancyGuard {
         address nftContract,
         uint256 itemId
     ) public payable nonReentrant {
-        
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
     }
 
 }
