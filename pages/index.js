e6fb90b5195377cae62080e52de8d26483ebf7e7
IqrarH <iqrarhussain471@gmail.com>
2022-03-01 19:13:49 +0500

    loadNFTs method added

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,31 @@ export default function Home() {
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider);
 
     const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId);
+      const meta = await axios.get(tokenUri);
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item;
+    }));
+
+    setNfts(items);
+    setLoadingState('loaded');
   }
+
+  if(loadingState === 'loaded' && !nfts.length) return (
+    <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>
+  )
 
   return (
     <div>
