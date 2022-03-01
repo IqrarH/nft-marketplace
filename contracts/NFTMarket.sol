f43b797bbc3a46bdeb37b47b7bf8a79198fde5bb
IqrarH <iqrarhussain471@gmail.com>
2022-03-01 12:32:46 +0500

    get my NFTs method added

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -94,6 +94,8 @@ contract NFTMarket is ReentrancyGuard {
         payable(owner).transfer(listingPrice);
     }
 
+
+    //Get All Unsold NFTs in the marketplace
     function fetchMarketItems() public view returns (MarketItem[] memory){
         uint itemCount = _itemIds.current();
         uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
@@ -109,6 +111,31 @@ contract NFTMarket is ReentrancyGuard {
                 currentIndex += 1;
             }
         }
+        return items;
+    }
+
+    function fetchMyNFTs() public view returns (MarketItem[] memory){
+        uint totalItemCount = _itemsIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for(uint i = 0; i < totalItemCount; i++){
+            if(idToMarketItem[i + 1].owner == msg.sender){
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+
+        for(uint i = 0; i < totalItemCount; i++){
+            if(idToMarketItem[i + 1].owner == msg.sender){
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
         return items;
     }
 }
