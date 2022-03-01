79a65aac2d57559f847f71ef4bea0f854b37ac3b
IqrarH <iqrarhussain471@gmail.com>
2022-03-01 18:24:32 +0500

    Navbar Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-6 text-pink-500">
+              Home
+            </a>
+          </Link>
+
+          <Link href="/create-item">
+            <a className="mr-6 text-pink-500">
+              Sell Digital Asset
+            </a>
+          </Link>
+          
+          <Link href="/create-item">
+            <a className="mr-6 text-pink-500">
+              My Digital Assets
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
